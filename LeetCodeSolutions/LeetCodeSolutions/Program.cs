293e922a14541a2ba0bb633908b4a3a49a205a15
Guli2111 <154041850+Guli2111@users.noreply.github.com>
2025-06-28 02:54:11 +0200

    Add solution for LeetCode 709: ToLowerCase

---
+++ LeetCodeSolutions/LeetCodeSolutions/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCodeSolutions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ToLOwerCase("Hello world!"));
+        }
+        public static string ToLOwerCase(string s)
+        {
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] >= 'A' && chars[i] <= 'Z')
+                {
+                    chars[i] = (char)(chars[i] + 32);
+                }
+            }
+            return new string(chars);
+        }
+    }
+}
